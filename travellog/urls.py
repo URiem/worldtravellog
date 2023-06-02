d5ae4ff1e415ebd47bcd0a5af5dabd84ea98f39a
URiem <>
2023-06-02 05:41:54 +0000

    Add Logentry detail view and get it to display correctly

--- travellog/urls.py
+++ travellog/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.LogentryList.as_view(), name='home'),
-    # path('<slug:slug>/', views.LogentryDetail.as_view(), name='logentry_detail'),
+    path('<slug:slug>/', views.LogentryDetail.as_view(), name='logentry_detail'),
 ]
--- travellog/views.py
+++ travellog/views.py
@@ -10,18 +10,18 @@ class LogentryList(generic.ListView):
     paginate_by = 6
 
 
-# class LogentryDetail(View):
+class LogentryDetail(View):
 
-#     def get(self, request, slug, *arg, **kwargs):
-#         queryset = Logentry.objects.filter(status=1)
-#         logentry = get_object_or_404(queryset, slug=slug)
-#         images = logentry.images
+    def get(self, request, slug, *arg, **kwargs):
+        queryset = Logentry.objects.filter(status=1)
+        logentry = get_object_or_404(queryset, slug=slug)
+        images = logentry.images
 
-#         return render(
-#             request,
-#             "logentry_detail.html",
-#             {
-#                 "logentry": logentry,
-#                 "images": images,
-#             }
-#         )
+        return render(
+            request,
+            "logentry_detail.html",
+            {
+                "logentry": logentry,
+                "images": images,
+            },
+        )
