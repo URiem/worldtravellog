bf446c6fa62b188287e78dafc5ae876f961e2a99
URiem <>
2023-06-02 03:55:49 +0000

    Add static files, css, js, images, add index.html and display Logentry List View

---
+++ travellog/urls.py
@@ -0,0 +1,7 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.LogentryList.as_view(), name='home'),
+    # path('<slug:slug>/', views.LogentryDetail.as_view(), name='logentry_detail'),
+]
--- travellog/views.py
+++ travellog/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
+from .models import Logentry
 
-# Create your views here.
+
+class LogentryList(generic.ListView):
+    model = Logentry
+    queryset = Logentry.objects.filter(status=1).order_by('-year')
+    template_name = 'index.html'
+    paginate_by = 6
+
+
+# class LogentryDetail(View):
+
+#     def get(self, request, slug, *arg, **kwargs):
+#         queryset = Logentry.objects.filter(status=1)
+#         logentry = get_object_or_404(queryset, slug=slug)
+#         images = logentry.images
+
+#         return render(
+#             request,
+#             "logentry_detail.html",
+#             {
+#                 "logentry": logentry,
+#                 "images": images,
+#             }
+#         )
